ac294faf6e01bbfecd8363479e69c624180ad111
Dragomir Matei-Mihai <mateimaat@gmail.com>
2025-12-28 22:54:46 +0200

    Completed Feb2023 exam

--- Older_tests/feb2023.hs
+++ Older_tests/feb2023.hs
@@ -1,4 +1,5 @@
-import GHC.Internal.TH.Lib (safe)
+import GHC.Internal.TH.Lib (safe, parS)
+import Data.Char (digitToInt)
 -- Problem 1
 -- norm is ad-hoc polymorphic
 
@@ -35,3 +36,71 @@ all3 (Node3 _ l m r) = all3 l && all3 m && all3 r
 all3 (Node2 _ _ _) = False
 uniform t = all2 t || all3 t
 
+-- Problem 3
+
+funcA (a,b) s
+    | a > b && s == "String" = digitToInt '7'
+
+-- adhoc polymorphism
+
+funcB :: Bool -> p -> p
+funcB b x = if b then x else x
+
+-- parametric polymorphism
+
+
+funcC :: Show a1 => [a2] -> a1 -> IO ()
+funcC [_] x = print x
+
+-- parametric and ad-hoc polymorphism
+
+
+
+funcD :: ((a1, b1), b2) -> [a2] -> ((a1, b2) -> a3) -> [a3]
+funcD ((x, _), b) [_] g = [g (x, b)]
+
+-- parametric polymorphism
+
+
+-- Problem 4
+
+readNumber :: String -> Maybe Int
+readNumber s = case reads s of 
+    [(n,"")] -> Just n 
+    x -> Nothing
+
+
+add :: (Monad m, Num b) => m b -> m b -> m b
+add mx my = do
+    x <- mx
+    y <- my
+    return (x+y)
+
+readAndAdd :: String -> String -> Maybe Int
+readAndAdd x y= do
+    mx <- readNumber x
+    my <- readNumber y
+    add (Just mx) (Just my)
+
+
+-- Problem 5
+
+-- parametric polymorphism
+
+repeatStrings _ [] = []
+repeatStrings [] _ = []
+repeatStrings (x:xs) (n:ns) = replicate n x ++ repeatStrings xs ns
+
+repeatStrings2 xs ns = foldr (++) [] (map (\(x,n) -> replicate n x) (zip xs ns))
+
+-- Problem 6
+
+triangles = [sum [0..n] | n <- [0..] ]
+
+
+triangles' = 0 : next 1 triangles'
+    where
+        next n (t:ts) = (t + n) : next (n + 1) ts
+
+
+triangles'' = map (\n -> foldr (+) 0 [1..n]) [0..]
